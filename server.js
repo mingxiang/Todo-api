db25a2cc770c05855164d80e898b93f982156a8c
mingxiang <mx3434@hotmail.com>
2016-01-23 20:09:42 +0800

    reset database contents

--- server.js
+++ server.js
@@ -271,7 +271,7 @@ app.post('/users/login', function(req, res){
    */
 });
 
-db.sequelize.sync({force: false}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
 //  db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function (){
     console.log('Express listen on port ' + PORT + '!');
